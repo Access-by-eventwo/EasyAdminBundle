de813349424ff2c631949fc4fa64fb80a97db980
Pedro Palmero <pedropalmero@gmail.com>
2026-05-27 10:01:25 +0200

    Use Route attribute instead of annotation

--- tests/Fixtures/AppTestBundle/Controller/OverridingEasyAdminController.php
+++ tests/Fixtures/AppTestBundle/Controller/OverridingEasyAdminController.php
@@ -4,15 +4,14 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Tests\Fixtures\AppTestBundle\Controlle
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class OverridingEasyAdminController extends AbstractController
 {
     /**
-     * @Route("/override_layout", name="override_layout")
-     *
      * @return Response
      */
+    #[Route('/override_layout', name: 'override_layout')]
     public function overrideLayout()
     {
         return $this->render('override_controller/layout.html.twig');
